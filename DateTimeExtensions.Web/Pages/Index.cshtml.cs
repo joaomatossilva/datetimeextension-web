519c11b7ca44ceb0abc40640bd43d0dc5834ffb3
João Filipe Matos Silva <joao@kspace.pt>
2024-01-21 18:01:13 +0000

    Set CurrentCulture same as UI

--- DateTimeExtensions.Web/Pages/Index.cshtml.cs
+++ DateTimeExtensions.Web/Pages/Index.cshtml.cs
@@ -10,14 +10,15 @@ public class IndexModel : PageModel
     public HolidayObservance[] YearObservances { get; set; }
     public int StartYear { get; set; } = DateTime.Today.Year - 1;
     public int EndYear { get; set; } = DateTime.Today.Year + 3;
-    public string Language { get; set; } = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
+    public string Language { get; set; }
 
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
-        Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+        Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+        Language = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
     }
 
     public void OnGet()
