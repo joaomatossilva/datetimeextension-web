8b03aac7d5b5fccb0bbd44a344cdc05388f4d6be
Joao Matos Silva <joaomatos.silva@farfetch.com>
2024-01-21 13:28:20 +0000

    Show Holidays on calendar

--- DateTimeExtensions.Web/Pages/Index.cshtml.cs
+++ DateTimeExtensions.Web/Pages/Index.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DateTimeExtensions.Web.Pages;
 
+using WorkingDays;
+
 public class IndexModel : PageModel
 {
+    public Dictionary<int, HolidayObservance[]> YearObservances { get; set; }
+    public int StartYear { get; set; } = DateTime.Today.Year - 1;
+    public int EndYear { get; set; } = DateTime.Today.Year + 3;
+
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -14,5 +20,22 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        var ci = new WorkingDayCultureInfo("pt-PT");
+        YearObservances = new Dictionary<int, HolidayObservance[]>();
+        for (int year = StartYear; year < EndYear; year++)
+        {
+            var observances = ci.GetHolidaysOfYear(year).Select(x => new HolidayObservance
+            {
+                Name = x.Name,
+                Date = x.GetInstance(year)
+            }).ToArray();
+            YearObservances[year] = observances;
+        }
+    }
+
+    public class HolidayObservance
+    {
+        public DateTime? Date { get; set; }
+        public string Name { get; set; }
     }
 }
